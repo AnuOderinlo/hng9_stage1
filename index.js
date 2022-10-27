88acc09965c2816ac5967d55ed2e461a5463afd0
Anuoluwapo <oderinloanuoluwapo@gmail.com>
2022-10-27 18:49:26 +0100

    feat: Completed stage 1 task in HNG9

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
+const dotenv = require("dotenv");
+
 const express = require("express");
 const bioRouter = require("./routes/route");
 const app = express();
+
+dotenv.config({ path: "./config.env" });
 
 app.use(express.json());
 
 app.use("/", bioRouter);
 
-const port = 3004;
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log("Successfully created a server on port:" + port);
 });
