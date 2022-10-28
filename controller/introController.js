3d681bfdf5e106fce1ebec769dd0f7de807d26e7
Anuoluwapo <oderinloanuoluwapo@gmail.com>
2022-10-28 07:40:06 +0100

    feat: Completed stage 1 task in HNG9

--- controller/introController.js
+++ controller/introController.js
@@ -6,7 +6,7 @@ exports.introController = (req, res) => {
       age: 31,
       bio: "My name is Anuoluwapo Oderinlo, a fullstack javascript developer with 2 - 3 years industrial experience in building fullstack application. I am vast in MERN stack. I was part of the finalist in HNG7. I decided to challenge myself in this internship and collaborate with others in building applications that will solve problems. Hopefully I will be of help and learn from everyone. Thank you",
     };
-    res.set("acess-control-allow-origin", "*");
+    res.set("access-control-allow-origin", "*");
     res.status(200).json({
       status: "success",
       message: "Successfully fetch data for Anuoluwapo Oderinlo",
